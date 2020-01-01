e7d02240a7f503541bb836ab03de396327fdcc4e
Cikkt <1685018404@qq.com>
2020-01-02 05:48:42 +0800

    Initial commit

---
+++ Test_001.py
@@ -0,0 +1,32 @@
+# -*- coding:utf8 -*-
+import pytest
+import allure
+
+
+class Test001(object):
+    @staticmethod
+    def setup_class():
+        print('========开始========')
+
+    @staticmethod
+    def teardown_class():
+        print('========结束========')
+
+    def test_001(self):
+        allure.attach('瞎ffの测试用例')
+        print('test001')
+
+    @allure.severity('BLOCKER')
+    @pytest.mark.skipif(3 > 1, reason='计算错误')
+    def test_002(self):
+        print('test002')
+
+    def test_003(self):
+        print('test666')
+
+    def test_004(self):
+        print('1')
+
+
+if __name__ == '__main__':
+    pytest.main()
